d6aee75a18aebb201f1629f1f821666da7cb9a4f
lhubeaux <louis.hubeaux@gmail.com>
2026-05-05 11:49:12 +0200

    ajout correctif

---
+++ exo1.py
@@ -0,0 +1,26 @@
+# Exercice console 1
+nom = input("Quel est votre nom de famille? \n")
+prenom = input("Quel est votre prénom? \n")
+print("Bonjour", prenom, nom, sep=" - ")
+
+# Exercice console 2
+print("Bonjour",end="!\n")
+print("Ceci", end=" ?\n")
+print("est un", end=" .\n")
+print("test", end=" :)\n")
+
+#Exercice console 3
+mot1 = input("Entrez un premier mot : ")
+mot2 = input("Entrez un deuxième mot : ")
+print(mot1, mot2, sep="")
+
+#Exercice console 4
+nbre1 = int(input("Entrez un premier nombre entier : \n"))
+nbre2 = int(input("Entrez un deuxième nombre entier : \n"))
+print(f"La somme des deux nombres vaut {nbre1 + nbre2}", end="!")
+
+#Exercice console 5
+val1 = 'Louis'
+val2 = 'Hubeaux'
+val3 = 'Python'
+print(val1, val2, val3, sep=".", end="!!")
