bbbf95879ff0f2236196c9abc1cea4fbf83691bd
lhubeaux <louis.hubeaux@gmail.com>
2026-05-05 13:16:36 +0200

    rename

---
+++ exo1+2_operateurs.py
@@ -0,0 +1,25 @@
+A = 12
+
+B = 3 * A + 8
+
+print("B=",B)
+
+C = B - 2 * A
+
+print("C=",C)
+
+D = (C + 25) * B
+
+print("D=",D)
+
+E = (A + B) % 10
+
+print("E=",E)
+
+F = (C * D)//(B + 6)
+
+print("F=",F)
+
+G = (D + E)//(F - 9)
+
+print("G=", G)
