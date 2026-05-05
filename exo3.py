e27563e06a76802b334fcee5751add58f2c79615
lhubeaux <louis.hubeaux@gmail.com>
2026-05-05 11:49:35 +0200

    corrections

---
+++ exo3.py
@@ -0,0 +1,4 @@
+#Exercice console 3
+mot1 = input("Entrez un premier mot : ")
+mot2 = input("Entrez un deuxième mot : ")
+print(mot1, mot2, sep="")
---
+++ exo4.py
@@ -0,0 +1,4 @@
+#Exercice console 4
+nbre1 = int(input("Entrez un premier nombre entier : \n"))
+nbre2 = int(input("Entrez un deuxième nombre entier : \n"))
+print(f"La somme des deux nombres vaut {nbre1 + nbre2}", end="!") #on peut aussi le faire avec une variable resultat
